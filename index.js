4e01e1a987c7fbdda9bd5e96a846c35ca6cf126f
protyasha201 <protyasha0999@gmail.com>
2021-04-02 16:15:22 +0600

    added delete and update

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const uri = `mongodb+srv://stairwayShop:StairwayHeaven201@cluster0.ywip5.mongodb
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db("stairwayHeaven").collection("products");
+  const ordersCollection = client.db("stairwayHeaven").collection("orders");
 
   app.post('/addProduct', (req, res) => {
     const newProduct = req.body;
@@ -27,21 +28,50 @@ client.connect(err => {
         res.send(result.insertedCount > 0);
       })
   })
-  app.get('/products', (req,res) => {
+
+  app.post('/orders', (req, res) => {
+    const orderedProduct = req.body;
+    ordersCollection.insertOne(orderedProduct)
+      .then(result => {
+        res.send(result.insertedCount > 0);
+      })
+  })
+
+  app.get('/products', (req, res) => {
     productsCollection.find({})
-    .toArray((err, documents) => {
-      res.send(documents);
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+
+  app.get('/product/:id', (req, res) => {
+    productsCollection.find({ _id: ObjectID(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      })
+  })
+
+  app.delete('/delete/:id', (req,res) => {
+    productsCollection.deleteOne({_id: ObjectID(req.params.id)})
+    .then(result => {
+      console.log(result);
     })
   })
 
-  app.get('/product/:id', (req,res) => {
-    productsCollection.find({_id: ObjectID(req.params.id)})
-    .toArray((err, documents) => {
-      res.send(documents[0]);
+  app.patch('/update/:id',(req,res) => {
+    productsCollection.updateOne({ _id: ObjectID(req.params.id)}, 
+    {
+      $set: {quantity: req.body.quantity}
+    }
+    )
+    .then(result => {
+      console.log(result);
     })
   })
+
+
   console.log("connected to database")
-    // client.close();
+  // client.close();
 });
 
 app.listen(port)
